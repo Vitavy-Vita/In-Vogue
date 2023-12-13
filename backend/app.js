aec57091a54c1d6b8f8ec591894ba3dcf074e8ad
Vitavy <127411106+Vitavy-Vita@users.noreply.github.com>
2023-12-13 19:46:59 +0100

    fixed stuff

--- backend/app.js
+++ backend/app.js
@@ -15,19 +15,26 @@ app.use(express.static(path.join(__dirname, "public")));
 
 dotenv.config();
 
-app.post("/api/v1/signup", (req, res) => {
-  console.log(userSchema);
+app.post('/api/v1/signup', (req, res) => {
+  // Affichage du corps de la requête dans la console
+  console.log(req.body);
 
   try {
-    
-    const user = userSchema.create(req.body);
-    res.status(201).json({
-      status: "success",
-      data: {
-        user,
-      },
+    // Création d'un nouvel utilisateur en utilisant le schéma d'utilisateur
+    // et les données provenant du corps de la requête
+    userSchema.create(req.body).then((user) => {
+      console.log(user);
+      // Envoi d'une réponse avec le statut 201 (créé) et les données de l'utilisateur
+      res.status(201).json({
+        status: 'success',
+        data: {
+          user,
+        },
+      })
     });
+
   } catch (error) {
+    // En cas d'erreur lors de la création de l'utilisateur, affiche l'erreur dans la console
     console.log(error);
   }
 });
