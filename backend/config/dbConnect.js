31c0ca05b620003aaff8f07a7b827288a20075e5
Vitavy <127411106+Vitavy-Vita@users.noreply.github.com>
2023-12-14 20:29:14 +0100

    all connected

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -2,10 +2,9 @@ const mongoose = require("mongoose");
 const chalk = require("chalk");
 
 const dbConnect = () => {
+  // `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@vitavy.tesxlpz.mongodb.net/${process.env.MONGO_DATABASE}`
   mongoose
-    .connect(
-      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@vitavy.tesxlpz.mongodb.net/${process.env.MONGO_DATABASE}`
-    )
+    .connect("mongodb://localhost:27017/invogue")
     .then(() => console.log(chalk.green("WE IN BOYS!")))
     .catch((err) => console.log(chalk.red(err)));
 };
