849904215f0a2c23d3855f1d62e2abfadfdaea13
Vitavy <127411106+Vitavy-Vita@users.noreply.github.com>
2023-12-14 20:17:51 +0100

    fixed stuff

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,8 @@ const logger = require("morgan");
 const dotenv = require("dotenv");
 
 const app = express();
-const userSchema = require('./models/userModel')
+const User = require("./models/userModel");
+
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -15,28 +16,49 @@ app.use(express.static(path.join(__dirname, "public")));
 
 dotenv.config();
 
-app.post('/api/v1/signup', (req, res) => {
-  // Affichage du corps de la requête dans la console
-  console.log(req.body);
+// Définition d'une route POST pour l'inscription d'un utilisateur
+app.post("/api/v1/signup", async (req, res) => {
+  // Création d'un nouvel utilisateur en utilisant le schéma d'utilisateur
+  // et les données provenant du corps de la requête.
+  // 'User.create(req.body)' renvoie une promesse qui résout à l'utilisateur créé.
+  // L'utilisation de 'await' permet d'attendre que la promesse soit résolue avant de continuer l'exécution du code.
+  // Cela signifie que 'user' contiendra l'utilisateur créé une fois que la promesse sera résolue.
 
-  try {
-    // Création d'un nouvel utilisateur en utilisant le schéma d'utilisateur
-    // et les données provenant du corps de la requête
-    userSchema.create(req.body).then((user) => {
-      console.log(user);
-      // Envoi d'une réponse avec le statut 201 (créé) et les données de l'utilisateur
+  User.create(req.body)
+    .then((user) => {
+      // Envoi d'une réponse avec le statut 201 (créé) et les données de l'utilisateur.
+      // Cette réponse est envoyée au client qui a fait la requête POST.
       res.status(201).json({
-        status: 'success',
+        status: "success",
         data: {
           user,
         },
-      })
+      });
+    })
+    .catch((err) => {
+      res.status(400).json({
+        status: "fail",
+        statusCode: err.statusCode,
+        message: err.message,
+      });
     });
-
-  } catch (error) {
-    // En cas d'erreur lors de la création de l'utilisateur, affiche l'erreur dans la console
-    console.log(error);
-  }
 });
 
+app.get("/api/v1/users", (req, res) => {
+  User.find().then((users) => {
+    res.status(200).json({
+      status: "success",
+      results: users.length,
+      data: {
+        users,
+      },
+    });
+  }).catch((err)=>{
+    res.status(500).json({
+      status:'error',
+      statusCode: err.statusCode,
+      message: err.message
+    })
+  })
+});
 module.exports = app;
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -16,8 +17,27 @@ const userSchema = new mongoose.Schema({
   password:{
     type:String,
     trim: true,
-    required:[true, "Please provide your password"]
+    required:[true, "Please provide your password"],
+    minlength:[8,'Password must be at least 8 characters']
+  },
+  passwordConfirm: {
+    type:String,
+    trim:true,
+    required:[true,'Please provide the same password'],
+    validate:{
+      validator:function(value){
+        return value === this.password
+      },
+      message: "Password are not the same"
+    },
+    minlength:[8,'Password must be at least 8 characters']
   }
 });
+
+userSchema.pre("save", async function (next){
+  this.password = await bcrypt.hash(this.password, 12)
+  this.passwordConfirm = undefined
+  next()
+})
 
 module.exports = mongoose.model.Users || mongoose.model('Users', userSchema)
